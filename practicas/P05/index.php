90c2bad2e87efe5ff77cb8ba5272656eca43ae43
Leonardo DOlores <leodoloresgar@gmail.com>
2025-09-10 12:15:49 -0600

    Se completo la actividad 2

--- practicas/P05/index.php
+++ practicas/P05/index.php
@@ -18,8 +18,8 @@
         $_element1;
         //$house*5;     // Invalida
         
-        echo '<h4>Respuesta:</h4>';   
-    
+        echo '<h4>Respuesta:</h4>';
+
         echo '<ul>';
         echo '<li>$_myvar es válida porque inicia con guión bajo.</li>';
         echo '<li>$_7var es válida porque inicia con guión bajo.</li>';
@@ -30,5 +30,37 @@
         echo '<li>$house*5 es inválida porque el símbolo * no está permitido.</li>';
         echo '</ul>';
     ?>
+
+
+    <h2>Ejercicio 2</h2>
+    <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
+    <?php
+        $a = "ManejadorSQL";
+        $b = "MySQL";
+        $c = &$a;
+        
+        echo "<h3>Primer bloque de asignaciones</h3>";
+echo "\$a = $a <br>";
+echo "\$b = $b <br>";
+echo "\$c = $c <br>";
+?>
+
+<?php
+$a = "PHP server";
+$b = &$a; // ahora b también referencia a a
+
+echo "<h3>Segundo bloque de asignaciones</h3>";
+echo "\$a = $a <br>";
+echo "\$b = $b <br>";
+echo "\$c = $c <br>";
+?>
+<?php
+echo "<h3>Explicación</h3>";
+echo "<p>En el primer bloque, \$c estaba referenciado a \$a, así que ambos mostraban el mismo valor inicial ManejadorSQL.<br>
+En el segundo bloque, cuando le asignamos a \$a con 'PHP server' y se hizo que \$b también fuera referencia a \$a,<br>
+entonces las tres variables apuntan al mismo valor.
+Por eso al mostrar el contenido, \$a, \$b y \$c imprimen lo mismo.</p>";
+?>
+
 </body>
 </html>
