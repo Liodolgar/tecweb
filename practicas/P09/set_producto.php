f2aff54e38d89457ca15b65e565ef5cc335c4e81
Leonardo DOlores <leodoloresgar@gmail.com>
2025-10-09 22:28:19 -0600

    Se agrego el formulario (actividad 2)

--- practicas/P09/set_producto.php
+++ practicas/P09/set_producto.php
@@ -8,18 +8,20 @@ $unidades = 1;
 $imagen   = 'img/imagen.png';
 
 /** SE CREA EL OBJETO DE CONEXION */
-@$link = new mysqli('localhost', 'root', '12345678a', 'marketzone');	
+@$link = new mysqli('localhost', 'root', '', 'marketzone');
 
 /** comprobar la conexión */
-if ($link->connect_errno) 
+if ($link->connect_errno)
 {
     die('Falló la conexión: '.$link->connect_error.'<br/>');
     /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
 }
 
 /** Crear una tabla que no devuelve un conjunto de resultados */
-$sql = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}')";
-if ( $link->query($sql) ) 
+$sql = "INSERT INTO productos (nombre, marca, modelo, precio, detalles, unidades, imagen)
+        VALUES ('{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}')";
+
+if ( $link->query($sql) )
 {
     echo 'Producto insertado con ID: '.$link->insert_id;
 }
